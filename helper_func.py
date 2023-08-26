5785c433807e1da32f8ab8a4849a7a46296e79f0
philo :) <139679726+philo-AI@users.noreply.github.com>
2023-08-26 15:57:23 +0300

    h

--- helper_func.py
+++ helper_func.py
@@ -197,9 +197,27 @@ def plot_time_series(timesteps, values, format='.', start=0, end=None, label=Non
     plt.ylabel("BTC Price")
     if label:
       plt.legend(fontsize=14)
-    plt.grid(True)    plt.grid(True)
+    plt.grid(True)
 
+def mean_absolute_scaled_error(y_true, y_pred):
   mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+  mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1])) 
   return mae / mae_naive_no_season
+
+def evaluate_preds(y_true, y_pred):
+  y_true = tf.cast(y_true, dtype=tf.float32)
+  y_pred = tf.cast(y_pred, dtype=tf.float32)
+  mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+  mse = tf.keras.metrics.mean_squared_error(y_true, y_pred)
+  rmse = tf.sqrt(mse)
+  mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+  def mean_absolute_scaled_error(y_true, y_pred):
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+    mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1])) 
+    return mae / mae_naive_no_season
+  mase = mean_absolute_scaled_error(y_true, y_pred)
+  return {"mae": mae.numpy(),
+          "mse": mse.numpy(),
+          "rmse": rmse.numpy(),
           "mape": mape.numpy(),
           "mase": mase.numpy()}
