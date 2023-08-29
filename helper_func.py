c43e458dadd92033c9afebe97dfe54ae53298acd
philo :) <139679726+philo-AI@users.noreply.github.com>
2023-08-29 12:26:24 +0300

    added a couple of function s

--- helper_func.py
+++ helper_func.py
@@ -4,6 +4,11 @@ import datetime
 import pandas as pd
 import numpy as np
 import sklearn 
+from PIL import Image
+from PIL import ImageOps
+import tempfile
+import six
+from six import BytesIO
 def image_imports():
         a = 'import numpy as np'
         b='import pandas as pd'
@@ -227,5 +232,88 @@ def evaluate_preds(y_true, y_pred):
           "rmse": rmse.numpy(),
           "mape": mape.numpy(),
           "mase": mase.numpy()}
-
   
+def download_and_resize_image(url, new_width, new_height):  
+    
+    '''
+    Fetches an image online, resizes it and saves it locally.
+    
+    Args:
+        url (string) -- link to the image
+        new_width (int) -- size in pixels used for resizing the width of the image
+        new_height (int) -- size in pixels used for resizing the length of the image
+        
+    Returns:
+        (string) -- path to the saved image
+    '''
+    
+    
+    # create a temporary file ending with ".jpg"
+    _, filename = tempfile.mkstemp(suffix=".jpg")
+    
+    # opens the given URL
+    response = six.moves.urllib.request.urlopen(url)
+    
+    # reads the image fetched from the URL
+    image_data = response.read()
+    
+    # puts the image data in memory buffer
+    image_data = BytesIO(image_data)
+    
+    # opens the image
+    pil_image = Image.open(image_data)
+    
+    # resizes the image. will crop if aspect ratio is different.
+    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+    
+    # converts to the RGB colorspace
+    pil_image_rgb = pil_image.convert("RGB")
+    
+    # saves the image to the temporary file created earlier
+    pil_image_rgb.save(filename, format="JPEG", quality=90)
+    
+    print("Image downloaded to %s." % filename)
+    
+    return filename
+
+def download_and_resize_image(url, new_width=256, new_height=256):
+    '''
+    Fetches an image online, resizes it and saves it locally.
+    
+    Args:
+        url (string) -- link to the image
+        new_width (int) -- size in pixels used for resizing the width of the image
+        new_height (int) -- size in pixels used for resizing the length of the image
+        
+    Returns:
+        (string) -- path to the saved image
+    '''
+    
+    
+    # create a temporary file ending with ".jpg"
+    _, filename = tempfile.mkstemp(suffix=".jpg")
+    
+    # opens the given URL
+    response = six.moves.urllib.request.urlopen(url)
+    
+    # reads the image fetched from the URL
+    image_data = response.read()
+    
+    # puts the image data in memory buffer
+    image_data = BytesIO(image_data)
+    
+    # opens the image
+    pil_image = Image.open(image_data)
+    
+    # resizes the image. will crop if aspect ratio is different.
+    pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.ANTIALIAS)
+    
+    # converts to the RGB colorspace
+    pil_image_rgb = pil_image.convert("RGB")
+    
+    # saves the image to the temporary file created earlier
+    pil_image_rgb.save(filename, format="JPEG", quality=90)
+    
+    print("Image downloaded to %s." % filename)
+    
+    return filename
