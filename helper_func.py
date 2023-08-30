402b900076184184bf935da70511c12be7b8dc94
philo :) <139679726+philo-AI@users.noreply.github.com>
2023-08-30 13:07:49 +0300

    added the training function for GANS

--- helper_func.py
+++ helper_func.py
@@ -8,6 +8,7 @@ from PIL import Image
 from PIL import ImageOps
 import tempfile
 import six
+from tensorflow import keras as ks
 from six import BytesIO
 def image_imports():
         a = 'import numpy as np'
@@ -309,3 +310,73 @@ def run_detector(detector, path):
     print(result["detection_scores"])
     print(result["detection_class_entities"])
     print(result["detection_boxes"])
+
+def training_model_for_GANs(genrator_optimezer, detector_optimizer, gen_loss, detector_loss,generator, detector,training_data,epochs,Callbacks=None):
+  class GAN_MODEL(ks.models.Model): 
+      def __init__(self, generator, discriminator, *args, **kwargs):
+          # Pass through args and kwargs to base class 
+          super().__init__(*args, **kwargs)
+          
+          # Create attributes for gen and disc
+          self.generator = generator 
+          self.discriminator =  detector
+          
+      def compile(self, genrator_optimezer, detector_optimizer, gen_loss, detector_loss, *args, **kwargs): 
+          # Compile with base class
+          super().compile(*args, **kwargs)
+          
+          # Create attributes for losses and optimizers
+          self.g_opt = genrator_optimezer
+          self.d_opt = detector_optimizer
+          self.g_loss = gen_loss
+          self.d_loss = detector_loss
+
+      def train_step(self, batch):
+          # Get the data 
+          real_images = batch
+          fake_images = self.generator(tf.random.normal((128, 128, 1)), training=False)
+          
+          # Train the discriminator
+          with tf.GradientTape() as d_tape: 
+              # Pass the real and fake images to the discriminator model
+              yhat_real = self.discriminator(real_images, training=True) 
+              yhat_fake = self.discriminator(fake_images, training=True)
+              yhat_realfake = tf.concat([yhat_real, yhat_fake], axis=0)
+              
+              # Create labels for real and fakes images
+              y_realfake = tf.concat([tf.zeros_like(yhat_real), tf.ones_like(yhat_fake)], axis=0)
+              
+              # Add some noise to the TRUE outputs
+              noise_real = 0.15*tf.random.uniform(tf.shape(yhat_real))
+              noise_fake = -0.15*tf.random.uniform(tf.shape(yhat_fake))
+              y_realfake += tf.concat([noise_real, noise_fake], axis=0)
+              
+              # Calculate loss - BINARYCROSS 
+              total_d_loss = self.d_loss(y_realfake, yhat_realfake)
+              
+          # Apply backpropagation - nn learn 
+          dgrad = d_tape.gradient(total_d_loss, self.discriminator.trainable_variables) 
+          self.d_opt.apply_gradients(zip(dgrad, self.discriminator.trainable_variables))
+          
+          # Train the generator 
+          with tf.GradientTape() as g_tape: 
+              # Generate some new images
+              gen_images = self.generator(tf.random.normal((128,128,1)), training=True)
+                                          
+              # Create the predicted labels
+              predicted_labels = self.discriminator(gen_images, training=False)
+                                          
+              # Calculate loss - trick to training to fake out the discriminator
+              total_g_loss = self.g_loss(tf.zeros_like(predicted_labels), predicted_labels) 
+              
+          # Apply backprop
+          ggrad = g_tape.gradient(total_g_loss, self.generator.trainable_variables)
+          self.g_opt.apply_gradients(zip(ggrad, self.generator.trainable_variables))
+          
+          return {"d_loss":total_d_loss, "g_loss":total_g_loss}
+  fashgan = GAN_MODEL(generator, detector)
+  fashgan.compile(genrator_optimezer, detector_optimizer, gen_loss, detector_loss)
+  if Callbacks == None:
+    return fashgan.fit(training_data,epochs=epochs)
+  else:
+    return fashgan.fit(training_data,epochs=epochs,callbacks=[Callbacks])
