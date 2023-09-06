6f733d4fce107568a483235b410cd84bc70277e3
philo :) <139679726+philo-AI@users.noreply.github.com>
2023-09-06 15:20:06 +0300

    added an obj detection helper fun

--- helper_func.py
+++ helper_func.py
@@ -405,3 +405,21 @@ def gan_preproccesing(data):
   # Reduces the likelihood of bottlenecking 
   ds = data.prefetch(64)
   return ds
+def object_detection_preprocessing(model,train_data,test_data,val_data=None,batch_size=None):
+  import numpy as np
+  import os
+  from tflite_model_maker.config import QuantizationConfig
+  from tflite_model_maker.config import ExportFormat
+  from tflite_model_maker import model_spec
+  from tflite_model_maker import object_detector
+
+  import tensorflow as tf
+  assert tf.__version__.startswith('2')
+
+  tf.get_logger().setLevel('ERROR')
+  from absl import logging
+  logging.set_verbosity(logging.ERROR)
+  mode = object_detector.create(train_data, model_spec=model, batch_size=batch_size, train_whole_model=True, validation_data=val_data)
+  mode.evaluate(test_data)
+  mode.export(export_dir='.')
+  return mode
